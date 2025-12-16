8553e245d623abb01200966e6fd565f49aaee08b
LourdesGrandotti <plgca.frre.utn.edu.ar@gmail.com>
2025-12-16 02:07:36 -0300

    Creando app usuario, Configuraciones de app usuario, Mysql - creación y enlace con el proyecto

--- ProyectoTurismo/Turismo/configuraciones/local.py
+++ ProyectoTurismo/Turismo/configuraciones/local.py
@@ -1,1 +1,17 @@
-from .settings import*
+from .settings import *
+
+# Configuración de base de datos para desarrollo local
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'turismo_db',  # Nombre de tu base de datos
+        'USER': 'root',
+        'PASSWORD': '12345',  # Tu contraseña de MySQL
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+# Opcional: Configuraciones adicionales para desarrollo
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
