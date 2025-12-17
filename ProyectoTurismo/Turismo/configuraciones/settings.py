1682ed74e436eb5da9b741e8ab378b2ebacc67a8
LourdesGrandotti <plgca.frre.utn.edu.ar@gmail.com>
2025-12-17 17:40:38 -0300

    2°Cambios en urls, index, settings

--- ProyectoTurismo/Turismo/configuraciones/settings.py
+++ ProyectoTurismo/Turismo/configuraciones/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '%-sd%x#un772tj^tlj0i0qljat=i36b@3r5ijpx!t_u9fzr+z^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.contacto.apps.ContactoConfig',
 
     'apps.posts',
-    'apps.contacto',
     'apps.usuario',
+
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Turismo.urls'
+
+LOGIN_URL = "usuario:login"
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"
+
 
 TEMPLATES = [
     {
@@ -124,12 +130,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
